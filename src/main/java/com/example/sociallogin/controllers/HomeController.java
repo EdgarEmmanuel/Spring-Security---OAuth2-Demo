080a970affc52cb7432eb7b40bf9429b3210897d
Edgar <mmnledgar@gmail.com>
2024-05-20 14:47:21 +0000

    First Commit

---
+++ src/main/java/com/example/sociallogin/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.sociallogin.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String home(){
+        return "Hello, Home";
+    }
+
+
+    @GetMapping("/secured")
+    public String secured()
+    {
+        return "Hello Secured";
+    }
+
+}
